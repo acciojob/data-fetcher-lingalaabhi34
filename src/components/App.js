ba71de53c9b819260375f7b1cc0897bcc8d8fdad
lingalaabhi34 <lingalaabhi34@gmail.com>
2024-02-25 12:45:55 +0530

    submitting code-Sun Feb 25 2024 12:45:53 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,34 @@
-
-import React,{useState,useEffect} from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import { data } from "cypress/types/jquery";
-// import { use } from "chai";
+
 const App = () => {
- const [datas,setdatas] = useState(null);
- useEffect(()=>{
-  getsomedata();
- })
- const getsomedata=()=>{
- axios.get("https://dummyjson.com/products").then(response => setdatas(response.data))
- }
+  const [datas, setDatas] = useState(null);
+
+  useEffect(() => {
+    getsomedata();
+  }, []);
+
+  const getsomedata = () => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((response) => setDatas(response.data))
+      .catch((err) => {
+        console.error("Error fetching data:", err);
+        setDatas("error");
+      });
+  };
+
   return (
     <div>
-        {!datas ? <h1>Loading...</h1> : <pre>
-          {JSON.stringify(datas, null, 2)}
-        </pre>}
-
+      {!datas ? (
+        <h1>Loading...</h1>
+      ) : datas === "error" ? (
+        <h1>Error fetching data</h1>
+      ) : (
+        <pre>{JSON.stringify(datas, null, 2)}</pre>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
