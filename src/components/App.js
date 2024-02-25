1194794c3f3dd9c5057cf77abcf06f8f39f719a8
lingalaabhi34 <lingalaabhi34@gmail.com>
2024-02-25 12:42:27 +0530

    submitting code-Sun Feb 25 2024 12:42:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React,{useState,useEffect} from "react";
 import './../styles/App.css';
-
+import axios from "axios";
+// import { data } from "cypress/types/jquery";
+// import { use } from "chai";
 const App = () => {
+ const [datas,setdatas] = useState(null);
+ useEffect(()=>{
+  getsomedata();
+ })
+ const getsomedata=()=>{
+ axios.get("https://dummyjson.com/products").then(response => setdatas(response.data))
+ }
   return (
     <div>
-        {/* Do not remove the main div */}
+        {!datas ? <h1>Loading...</h1> : <pre>
+          {JSON.stringify(datas, null, 2)}
+        </pre>}
+
     </div>
   )
 }
